390c9af8998de3f91037ec0a4c8ebb26d30401d2
Gabe Jensen <thegabejensen@gmail.com>
2021-08-08 14:06:33 -0700

    Restyled home page phew...

--- src/components/RadioTab.js
+++ src/components/RadioTab.js
@@ -1,32 +1,59 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
+import Spacer from './Spacer';
+
+const StyledRadioTab = styled.div`
+  width: 800px;
+  margin: auto;
+`;
+
+const StyledRadioButtonsList = styled.div`
+  display: flex;
+`;
+
+const StyledRadioButtonWrapper = styled.div`
+  margin-right: 10px;
+`;
+
+const StyledRadioInput = styled.input`
+  margin-right: 5px;
+  cursor: pointer;
+`;
+
+const StyledLabel = styled.label`
+  cursor: pointer;
+`;
 
 export default function RadioTab({ options }) {
   const [method, setMethod] = useState(options[0].name);
 
   return (
-    <div>
-      {options.map(option => {
-        return (
-          <div className="inline mr-4" key={option.name}>
-            <input
-              type="radio"
-              name="gif_input_method"
-              value={option.name}
-              className="mr-1 cursor-pointer"
-              checked={method === option.name}
-              onChange={() => setMethod(option.name)}
-            />
-            <label
-              htmlFor="gif_input_method"
-              onClick={() => setMethod(option.name)}
-              className="cursor-pointer"
-            >{option.displayName}</label>
-          </div>
-        );
-      })}
-      <div className="mt-3">
+    <StyledRadioTab className="__StyledRadioTab">
+      <StyledRadioButtonsList className="__StyledRadioButtonsList">
+        {options.map(option => {
+          return (
+            <StyledRadioButtonWrapper className="__StyledRadioButtonWrapper" key={option.name}>
+              <StyledRadioInput
+                type="radio"
+                name="gif_input_method"
+                value={option.name}
+                checked={method === option.name}
+                onChange={() => setMethod(option.name)}
+              />
+              <StyledLabel
+                htmlFor="gif_input_method"
+                onClick={() => setMethod(option.name)}
+              >
+                {option.displayName}
+              </StyledLabel>
+            </StyledRadioButtonWrapper>
+          );
+        })}
+      </StyledRadioButtonsList>
+      <Spacer height="14px" />
+      <div>
         {options.find(x => x.name === method).content}
       </div>
-    </div >
+    </StyledRadioTab>
   );
 };
